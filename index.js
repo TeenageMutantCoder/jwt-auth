fda84c1b30e2fa4d25a16cb12855ea15999a2af4
Stevon Wright <stevonisepic@gmail.com>
2021-11-01 22:11:14 -0400

    Connected to database

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// Connect to database
+require("dotenv").config();
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_URI, (err) => {
+  if (err) throw err;
+});
+
 // Middleware
 app.use(express.json());
 
