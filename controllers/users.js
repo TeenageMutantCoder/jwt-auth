3bf09cff1242848bb580245fd165a4a24fe0b923
Stevon Wright <stevonisepic@gmail.com>
2021-11-05 22:11:44 -0400

    Added status codes to responses

--- controllers/users.js
+++ controllers/users.js
@@ -1,13 +1,16 @@
+const { StatusCodes } = require("http-status-codes");
 const User = require("../models/User");
 
 async function getAllUsers(req, res) {
   const users = await User.find({});
-  return res.json({ msg: "Successfully retrieved users", data: users });
+  return res
+    .status(StatusCodes.OK)
+    .json({ msg: "Successfully retrieved users", data: users });
 }
 
 async function deleteAllUsers(req, res) {
   const deletedUsers = await User.deleteMany({});
-  return res.json({
+  return res.status(StatusCodes.OK).json({
     msg: "Successfully deleted all users",
     data: deletedUsers,
   });
@@ -17,56 +20,72 @@ async function createUser(req, res) {
   const { username, password } = req.body;
   const userExists = await User.exists({ username: username });
   if (userExists) {
-    return res.json({
+    return res.status(StatusCodes.FORBIDDEN).json({
       msg: `Failed to create user. User already exists with username "${username}"`,
     });
   }
-  const newId = await getNewId();
+  const newId = await User.getNewId();
   const newUser = new User({
     _id: newId,
     username: username,
     password: password,
   });
   await newUser.save();
-  return res.json({ msg: "Successfully created user", data: newUser });
+  return res
+    .status(StatusCodes.CREATED)
+    .json({ msg: "Successfully created user", data: newUser });
 }
 
 async function findUser(req, res) {
   const { username, password } = req.body;
   const user = await User.find({ username: username, password: password });
   if (!user) {
-    return res.json({
+    return res.status(StatusCodes.NOT_FOUND).json({
       msg: `Failed to find user with username "${username}" and password "${password}"`,
     });
   }
-  return res.json({ msg: "Successfully found user", data: user });
+  return res
+    .status(StatusCodes.OK)
+    .json({ msg: "Successfully found user", data: user });
 }
 
 async function findUserById(req, res) {
   const id = parseInt(req.params.id);
   const user = await User.findById(id);
   if (!user) {
-    return res.json({ msg: `Failed to find user with id "${id}"` });
+    return res
+      .status(StatusCodes.NOT_FOUND)
+      .json({ msg: `Failed to find user with id "${id}"` });
   }
-  return res.json({ msg: "Successfully found user", data: user });
+  return res
+    .status(StatusCodes.OK)
+    .json({ msg: "Successfully found user", data: user });
 }
 
 async function deleteUser(req, res) {
   const id = parseInt(req.params.id);
   const deletedUser = await User.findByIdAndDelete(id);
   if (!deletedUser) {
-    return res.json({ msg: `User with id "${id}" does not exist` });
+    return res
+      .status(StatusCodes.NOT_FOUND)
+      .json({ msg: `User with id "${id}" does not exist` });
   }
-  return res.json({ msg: `Successfully deleted user with id "${id}"` });
+  return res
+    .status(StatusCodes.OK)
+    .json({ msg: `Successfully deleted user with id "${id}"` });
 }
 
 async function getUserData(req, res) {
   const id = parseInt(req.params.id);
   const user = await User.findById(id);
   if (!user) {
-    return res.json({ msg: `Failed to find user with id "${id}"` });
+    return res
+      .status(StatusCodes.NOT_FOUND)
+      .json({ msg: `Failed to find user with id "${id}"` });
   }
-  return res.json({ msg: "Successfully returned user data", data: user.data });
+  return res
+    .status(StatusCodes.OK)
+    .json({ msg: "Successfully returned user data", data: user.data });
 }
 
 async function editUserData(req, res) {
@@ -75,13 +94,15 @@ async function editUserData(req, res) {
   if (data["_id"]) delete data["_id"];
   const userExists = await User.exists({ _id: id });
   if (!userExists) {
-    return res.json({ msg: `Failed to find user with id "${id}"` });
+    return res
+      .status(StatusCodes.NOT_FOUND)
+      .json({ msg: `Failed to find user with id "${id}"` });
   }
   const updatedUser = await User.findByIdAndUpdate(id, data, {
     returnDocument: "after",
     runValidators: true,
   });
-  return res.json({
+  return res.status(StatusCodes.OK).json({
     msg: `Successfully updated user with id "${id}"`,
     data: updatedUser,
   });
